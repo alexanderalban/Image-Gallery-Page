373ab913af4a6bf1b99e04fe34240c25503c7da7
alexanderalban <albanalexander@gmail.com>
2020-10-02 20:31:02 -0500

    gallery squish added

---
+++ gallery.js
@@ -0,0 +1,11 @@
+const minsize = () => {
+    document.getElementById("main-container").style.width = "25%";
+} 
+
+const midsize = () => {
+    document.getElementById("main-container").style.width = "50%";
+}
+
+const maxsize = () => {
+    document.getElementById("main-container").style.width = "100%";
+}
